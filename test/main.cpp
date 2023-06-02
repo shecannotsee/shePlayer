c1e60640a528984be668cf813bf27de28c70a516
shecannotsee <shecannotsee@163.com>
2023-06-02 17:10:55 +0800

    [feat]雷神原版代码

--- test/main.cpp
+++ test/main.cpp
@@ -3,12 +3,13 @@
 //
 #include <iostream>
 #include "m1_simple_player_test.h"
+#include "m2_baseon_sdl_player_test.h"
 
 int main() {
   std::cout << "test start.\n";
 
-  m1_simple_player_test::main();
-
+//  m1_simple_player_test::main();
+  m2_baseon_sdl_player_test::main();
   std::cout << "test end.\n";
   return 0;
 };
