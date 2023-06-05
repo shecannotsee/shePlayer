71fe836b46e61d4c960529b3023812d9c1308068
shecannotsee <shecannotsee@163.com>
2023-06-05 22:45:36 +0800

    [test]将输出颜色独立到一个头文件中用来处理循环依赖

--- test/main.cpp
+++ test/main.cpp
@@ -2,15 +2,9 @@
 // Created by shecannotsee on 23-6-2.
 //
 #include <iostream>
+#include "print_color.h"
 #include "m1_video_decode_test.h"
 #include "m2_play_with_sdl2_test.h"
-
-const std::string RESET_COLOR   = "\033[0m";
-const std::string RED_COLOR     = "\033[31m";
-const std::string GREEN_COLOR   = "\033[32m";
-const std::string YELLOW_COLOR  = "\033[33m";
-const std::string PURPLE_COLOR  = "\033[35m";
-const std::string BLUE_COLOR    = "\033[34m";
 
 int main() {
   std::cout << "test start.\n";
---
+++ test/print_color.h
@@ -0,0 +1,17 @@
+//
+// Created by shecannotsee on 23-6-5.
+//
+
+#ifndef SHEPLAYER_PRINT_COLOR_H
+#define SHEPLAYER_PRINT_COLOR_H
+
+#include <string>
+
+const std::string RESET_COLOR   = "\033[0m";
+const std::string RED_COLOR     = "\033[31m";
+const std::string GREEN_COLOR   = "\033[32m";
+const std::string YELLOW_COLOR  = "\033[33m";
+const std::string PURPLE_COLOR  = "\033[35m";
+const std::string BLUE_COLOR    = "\033[34m";
+
+#endif //SHEPLAYER_PRINT_COLOR_H
