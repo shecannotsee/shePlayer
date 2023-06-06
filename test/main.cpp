8dd8939733ebedc83b0a83863cd223f91f77d622
shecannotsee <shecannotsee@163.com>
2023-06-07 00:22:47 +0800

    [test]添加一个新的测试用例

--- test/main.cpp
+++ test/main.cpp
@@ -5,12 +5,14 @@
 #include "print_color.h"
 #include "m1_video_decode_test.h"
 #include "m2_play_with_sdl2_test.h"
+#include "m3_sdl2_show_test.h"
 
 int main() {
   std::cout << "test start.\n";
 
 //  m1_video_decode_test::main();
-  m2_play_with_sdl2_test::main();
+//  m2_play_with_sdl2_test::main();
+  m3_sdl2_show_test::main();
 
   std::cout << "test end.\n";
   return 0;
